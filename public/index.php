0bb9abd7ca22ec26da1928ebd9f563f0b388a4a2
Lucemtis <128071639+Lucemtis@users.noreply.github.com>
2023-03-16 18:25:28 +0100

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+   <html lang="fr"> 
+    <head>
+       <meta charset="UTF-8" />
+       <title>Squelette</title>
+       <meta name="author" content="MOIO"/>
+       <meta name="keywords" content="TEST"/>
+       <meta name="description" content="Squelette"/>
+    </head>
+    <body>
+        
+    </body>
+</html>
